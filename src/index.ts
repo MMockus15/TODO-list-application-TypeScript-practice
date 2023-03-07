57743f74ab26697845205a88a0c60e4428a2579a
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2023-03-06 19:39:38 -0700

    task checkbox not appending

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-// import { v4 as uuidV4 } from 'uuid'
+import { v4 as uuidV4 } from 'uuid'
 
-// console.log(uuidV4());
+type Task = {
+  id: string 
+  title: string 
+  completed: boolean 
+  createdAt: Date 
+}
 
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("#new-task-form") as HTMLFormElement | null
@@ -11,5 +16,22 @@ form?.addEventListener("submit", e => {
 
   if (input?.value == "" || input?.value == null) return
 
-  input.value
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  }
+
+  addListItem(newTask)
 })
+
+function addListItem(task: Task ) {
+const item = document.createElement("li")
+const label = document.createElement("label")
+const checkbox = document.createElement("input")
+checkbox.type = "checkbox"
+label.append(checkbox, task.title)
+item.append(label)
+list?.append(item)
+}
